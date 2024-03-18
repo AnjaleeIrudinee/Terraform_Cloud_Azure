4f9ddd2cd5f15b77d6069b8bce8f0df4b1e4db94
A.M.A.I.Bandula Rathna <72916330+AnjaleeIrudinee@users.noreply.github.com>
2024-03-18 21:38:07 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -6,6 +6,14 @@ terraform {
       version = "~> 3.0.2"
     }
   }
+# Update this block with the location of your terraform state file
+  backend "azurerm" {
+    resource_group_name  = "devops1"
+    storage_account_name = "terraformgithubactions1"
+    container_name       = "tfstate1"
+    key                  = "demo.terraform.tfstate1"
+}
+
   cloud {
     organization = "kochasoft_test"
     workspaces {
